3d470688fa07371d938eb607764eb1c8e06792fd
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-01-27 13:43:35 -0800

    Horizontal Movement

---
+++ Assets/_Scripts/HorizontalMovementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controls movement for left/right controls
+public class HorizontalMovementController : MonoBehaviour
+{
+    public Rigidbody2D rigidB;
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void MoveLeft()
+    {
+        rigidB.velocity = new Vector2(-movementSpeed, rigidB.velocity.y);
+    }
+
+    public void MoveRight()
+    {
+        rigidB.velocity = new Vector2(movementSpeed, rigidB.velocity.y);
+    }
+}
