62928aba8c02cb04fc5b622722a828285f253a05
Ken Miller <ken.miller521@gmail.com>
2018-01-28 14:41:36 -0800

    Last touches

---
+++ Assets/_Scripts/SoundPrefs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SoundPrefs : MonoBehaviour {
+    public AudioSource mainMenuAudioSource;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MasterVolume(Slider slider)
+    {
+        AudioListener.volume = slider.value;
+        PlayerPrefs.SetFloat("Master", slider.value);
+    }
+    public void SFXVolume(Slider slider)
+    {
+        PlayerPrefs.SetFloat("SFX", slider.value);
+    }
+    public void MusicVolume(Slider slider)
+    {
+        mainMenuAudioSource.volume = slider.value;
+        PlayerPrefs.SetFloat("Music", slider.value);
+    }
+}
