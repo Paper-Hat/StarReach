122fffa70282b53c991636c92222157ba0d54ba0
Paper-Hat <justinkristian@gmail.com>
2018-01-27 16:41:16 -0800

    Basic PartnerBoost, TimerController, and PlayerController implementations

---
+++ Assets/_Scripts/PartnerBoost.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Attached to Booster game object - Activated by opposite player, causes an increase in
+the next force transfer*/
+public class PartnerBoost : MonoBehaviour {
+    public float boost;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        other.transform.parent.gameObject.GetComponent<TimerController>().goodTiming = true;
+        other.transform.parent.gameObject.GetComponent<TimerController>().boostForce = boost;
+    }
+    void OnTriggerExit2D(Collider2D other)  {   other.transform.parent.gameObject.GetComponent<TimerController>().goodTiming = false;   }
+}
