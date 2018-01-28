3a700a53570b51d8ba4ea0edc237c8f7606d5879
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-01-28 14:44:18 -0800

    Lore

---
+++ Assets/_Scripts/Lore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lore : MonoBehaviour
+{
+    public bool done;
+	// Use this for initialization
+	void Start ()
+    { 
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(done)
+        {
+            SceneManager.LoadScene("RubenTestScene1");
+            print("done");
+        }
+	}
+}
