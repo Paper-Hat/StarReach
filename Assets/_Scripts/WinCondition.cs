a66f0d022d05d675fd460a2590abfb369a9ebf88
Eric <eric.lee0051@gmail.com>
2018-01-28 00:43:13 -0800

    win condition

--- Assets/_Scripts/WinCondition.cs
+++ Assets/_Scripts/WinCondition.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour
 {
-    
+    private bool win;
+
+    void Awake()
+    {
+        win = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        win = true;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
 }
